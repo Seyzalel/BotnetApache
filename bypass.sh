994c07aee5dae69af843f93f4e7bcc4eca09bd1f
Seyzalel Menabel <127889263+Seyzalel@users.noreply.github.com>
2024-04-17 00:04:31 -0300

    Update bypass.sh

--- bypass.sh
+++ bypass.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while true; do
-    python app.py &
+    python app.py https://agrcbt.unicard.pt/ &
     PID=$!
     # Dorme por um período aleatório entre 7 a 10 segundos
     sleep $((7 + RANDOM % 4))
